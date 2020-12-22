ce1d994eb274bbafe23f54e48a8e553ce446365f
ramilamparo <ramilamparo@gmail.com>
2020-12-22 12:26:08 +0400

    Format.

--- src/config/securepath.ts
+++ src/config/securepath.ts
@@ -1,7 +1,7 @@
 const { SECUREPATH_BASE_URL } = process.env;
 
 if (SECUREPATH_BASE_URL) {
-  throw new Error("Environment variable SECUREPATH_BASE_URL is not set!");
+	throw new Error("Environment variable SECUREPATH_BASE_URL is not set!");
 }
 
 export const baseUrl = SECUREPATH_BASE_URL;
--- src/utils/ResponseBuilder.ts
+++ src/utils/ResponseBuilder.ts
@@ -1,93 +1,93 @@
 import { Response } from "express";
 import {
-  AuthNeededException,
-  CredentialsException,
-  DeviceExistingException,
+	AuthNeededException,
+	CredentialsException,
+	DeviceExistingException
 } from "securepath-api";
 
 export enum StatusCode {
-  OK = "OK",
-  UNKNOWN_ERROR = "UNKNOWN_ERROR",
-  INVALID_ACTION = "INVALID_ACTION",
-  UNAUTHENTICATED = "UNAUTHENTICATED",
+	OK = "OK",
+	UNKNOWN_ERROR = "UNKNOWN_ERROR",
+	INVALID_ACTION = "INVALID_ACTION",
+	UNAUTHENTICATED = "UNAUTHENTICATED"
 }
 
 export interface ServerResponseMeta {
-  code: StatusCode;
-  errors: Array<FieldError>;
-  success: boolean;
-  message: string;
+	code: StatusCode;
+	errors: Array<FieldError>;
+	success: boolean;
+	message: string;
 }
 
 export type FieldError =
-  | string
-  | {
-      key: string;
-      value: string;
-    };
+	| string
+	| {
+			key: string;
+			value: string;
+	  };
 
 export class ResponseBuilder<T> {
-  constructor(
-    public data: T | null = null,
-    public meta: ServerResponseMeta = {
-      success: false,
-      message: "Unknown server error",
-      errors: [],
-      code: StatusCode.UNKNOWN_ERROR,
-    }
-  ) {}
+	constructor(
+		public data: T | null = null,
+		public meta: ServerResponseMeta = {
+			success: false,
+			message: "Unknown server error",
+			errors: [],
+			code: StatusCode.UNKNOWN_ERROR
+		}
+	) {}
 
-  public setData = (data: T) => {
-    this.data = data;
-  };
+	public setData = (data: T) => {
+		this.data = data;
+	};
 
-  public setSuccess = (success: boolean) => {
-    this.meta.success = success;
-  };
+	public setSuccess = (success: boolean) => {
+		this.meta.success = success;
+	};
 
-  public appendError = (error: FieldError) => {
-    this.meta.errors.push(error);
-  };
+	public appendError = (error: FieldError) => {
+		this.meta.errors.push(error);
+	};
 
-  public setCode = (code: StatusCode) => {
-    this.meta.code = code;
-  };
+	public setCode = (code: StatusCode) => {
+		this.meta.code = code;
+	};
 
-  public setMessage = (message: string) => {
-    this.meta.message = message;
-  };
+	public setMessage = (message: string) => {
+		this.meta.message = message;
+	};
 
-  public handleExpressError = (e: Error, res: Response) => {
-    if (e instanceof AuthNeededException) {
-      this.setCode(StatusCode.UNAUTHENTICATED);
-      this.setMessage(e.message);
-      res.status(401);
-    } else if (e instanceof DeviceExistingException) {
-      this.setCode(StatusCode.INVALID_ACTION);
-      this.setMessage(e.message);
-      res.status(403);
-    } else if (e instanceof CredentialsException) {
-      this.setCode(StatusCode.INVALID_ACTION);
-      this.setMessage("Invalid credentails");
-      res.status(403);
-    } else {
-      console.error(e);
-      this.setMessage(e.message);
-      res.status(500);
-    }
-  };
+	public handleExpressError = (e: Error, res: Response) => {
+		if (e instanceof AuthNeededException) {
+			this.setCode(StatusCode.UNAUTHENTICATED);
+			this.setMessage(e.message);
+			res.status(401);
+		} else if (e instanceof DeviceExistingException) {
+			this.setCode(StatusCode.INVALID_ACTION);
+			this.setMessage(e.message);
+			res.status(403);
+		} else if (e instanceof CredentialsException) {
+			this.setCode(StatusCode.INVALID_ACTION);
+			this.setMessage("Invalid credentails");
+			res.status(403);
+		} else {
+			console.error(e);
+			this.setMessage(e.message);
+			res.status(500);
+		}
+	};
 
-  public handleExpressSuccess = (message: string, res: Response) => {
-    this.setMessage(message);
-    this.setCode(StatusCode.OK);
-    this.setSuccess(true);
-    res.status(200);
-  };
+	public handleExpressSuccess = (message: string, res: Response) => {
+		this.setMessage(message);
+		this.setCode(StatusCode.OK);
+		this.setSuccess(true);
+		res.status(200);
+	};
 
-  public toObject = () => {
-    return {
-      data: this.data,
-      ...this.meta,
-    };
-  };
+	public toObject = () => {
+		return {
+			data: this.data,
+			...this.meta
+		};
+	};
 }
